84ba8e9d729521911ea82df27feb65cefae42ed6
bobsfriend12 <calebsethherring+github@gmail.com>
2026-01-07 21:54:42 -0600

    add cms

--- .eleventy.js
+++ .eleventy.js
@@ -5,22 +5,25 @@ import path from "path";
 
 export default async eleventyConfig => {
     eleventyConfig.addPassthroughCopy("src/static");
+    eleventyConfig.addPassthroughCopy("src/admin/*");
     eleventyConfig.addWatchTarget("src/sass");
     eleventyConfig.addTemplateFormats("njk");
     eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
-    
+
     eleventyConfig.setBrowserSyncConfig({
         https: true,
         callbacks: {
-            ready: function(err, bs) {
+            ready: function (err, bs) {
                 bs.addMiddleware("*", (req, res) => {
-                    const content_404 = fs.readFileSync('public/404.html');
-                    res.writeHead(404, {"Content-Type": "text/html; charset=UTF-8"});
+                    const content_404 = fs.readFileSync("public/404.html");
+                    res.writeHead(404, {
+                        "Content-Type": "text/html; charset=UTF-8",
+                    });
                     res.write(content_404);
                     res.end();
                 });
-            }
-        }
+            },
+        },
     });
 
     const md = new mdIt({
